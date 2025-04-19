6d3d87ee2dd4ec80a564df0c2c133b4f7b7ab10e
Abhishek kumar jha <jha.abhishek001@gmail.com>
2025-04-19 23:29:07 +0530

    modified for frontend connection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOWED_ORIGINS = [
+    "http://34.226.245.246",
+    "http://localhost:3000",
+]
+
+# If using credentials
+CORS_ALLOW_CREDENTIALS = True
+
+# settings.py
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static')]
