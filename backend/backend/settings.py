0f9d42aae8551a584a187b52fa101d1d89eaaa04
Abhishek kumar jha <jha.abhishek001@gmail.com>
2025-04-19 20:02:49 +0530

    aws commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&h&$m$^*(m*xv!o)y6a)5mp&4gxsa#j+28sfxukkag3!mlzwq-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['your-ec2-public-ip', 'localhost']
+ALLOWED_HOSTS = ['34.226.245.246', 'localhost']
 
 
 # Application definition
